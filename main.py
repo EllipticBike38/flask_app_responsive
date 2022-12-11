bb8e31bc5ca6dda8388052d026b3efa3225273bb
Andrea Mazzini <andrea.mazzini00@gmail.com>
2022-12-11 15:31:59 +0100

    adjusted menu logic

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 from flask import Flask, render_template
 from flaskwebgui import FlaskUI
+from api import db_init, api
 
-app = Flask(__name__)
+app = Flask(__name__, subdomain_matching=True)
+app.config['SERVER_NAME'] = "mini-desktop-pc.homenet.telecomitalia.it:5555"
+app.register_blueprint(api, subdomain="api")
 
 
-
+context={'menus':[
+        ('Home','#'),
+        ('Contacts','#'),
+        ('Settings','#'),
+        ('About Us','#'),
+        ('Login','login')
+    ]}
 @app.route('/')
 def index():
-    context={'menus':[
-        'Home',
-        'Contacts',
-        'Settings',
-        'About Us',
-        'Login'
-    ]}
     return render_template('index.html',**context)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html',**context)
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html',**context)
 
 if __name__ == "__main__":
     # FlaskUI(app=app, server="flask",width=500, height=500).run()
+    db_init()
     app.run("0.0.0.0",5555,True)
 
     
